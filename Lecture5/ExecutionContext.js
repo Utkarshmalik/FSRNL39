0930b054dafb9c8dfd4a7297ed0b369f7b37d94e
utmalik <utmalik@amazon.com>
2023-11-05 18:53:45 +0530

    Execution Context , GEC, FEC , SCope, Scope chaining , var let const

---
+++ Lecture5/ExecutionContext.js
@@ -0,0 +1,63 @@
+// var x = 10;
+
+// function timesTen(a){
+//     return a * 10;
+// }
+
+// var y = timesTen(x);
+
+// console.log(y);
+
+
+// function add(a, b) {
+//     return a + b;
+// }
+
+// function average(a, b) {
+//     return add(a, b) / 2;
+// }
+
+// var x = average(10, 20);
+
+// console.log(x);
+
+
+// console.log(y);
+// var x=1;
+
+
+
+
+
+
+// var x=add(3,5);
+// console.log(x);
+
+// function add(a,b){
+//     return a+b;
+// }
+
+
+
+
+// var x=add(3,5);
+
+// console.log(x);
+
+
+// var  add = function(a,b){
+//     return a+b;
+// }
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ Lecture5/scope.js
@@ -0,0 +1,118 @@
+// var message = 'Hi';
+
+// function say() {
+//     var message = 'Hello';
+//     console.log(message);
+// }
+
+// say();
+// console.log(message);
+
+
+
+
+// var message = 'Hi';
+
+// function say() {
+//     console.log(message);
+// }
+
+// say();
+
+
+// var y = 20;
+
+// function bar() {
+
+//     function baz() {  
+//         console.log(y);
+//     }
+
+//     baz();
+// }
+
+// bar();
+
+
+
+//    var greeter = "hey hi";
+    
+//     function newFunction() {
+//         var hello = "hello";
+//     }
+
+//     newFunction();
+
+//     console.log(hello);
+
+
+    //   var greeter = "hey hi";
+    // var greeter = "say Hello instead";
+    // console.log(greeter);
+
+    //     var greeter = "hey hi";
+    // greeter = "say Hello instead";
+
+
+
+
+
+
+    /*
+     Var
+     function scoped 
+     re declared and updated 
+     Hoisting : Hoisted but partially i:e with undefined as a value
+
+     Let
+
+     Block scoped
+     update but not re declare
+     Hoisting : Hoisted but they lie in TDZ , cant be accessed before they are defined 
+
+
+     Const 
+     Block scoped 
+     no update, no re declare
+      Hoisting : Hoisted but they lie in TDZ , cant be accessed before they are defined 
+     */
+
+
+
+    // var greeter = "hey hi";
+    // var times = 4;
+
+    // if (times > 3) {
+    //     let greeter = "say Hello instead"; 
+    // }
+    
+    // console.log(greeter);
+
+
+
+//   let greeting = "say Hi";
+//    let times = 4;
+
+//    if (times > 3) {
+//         let hello = "say Hello instead";
+//         console.log(hello);
+//    }
+
+//    console.log(hello);
+
+
+// let x=4;
+//  x=10;
+
+
+
+// console.log(x);
+// let x=10;
+
+// const greeting = "say Hi";
+//  greeting = "say Hello instead";
+
+
+//  console.log(x);
+//  const x=5;
+
